22a8bbe6404f93fb31936355fecc31b86772b358
Lucas Cavalcante dos Santos <cavalcanteprofissional@outlook.com>
2026-03-20 18:06:19 -0300

    debug: add test page

---
+++ src/app/test/page.tsx
@@ -0,0 +1,9 @@
+export default function TestPage() {
+  return (
+    <div style={{ padding: "40px", textAlign: "center" }}>
+      <h1>🧪 Teste</h1>
+      <p>Se você está vendo esta mensagem, o Next.js está funcionando!</p>
+      <a href="/login" style={{ color: "blue" }}>Ir para Login</a>
+    </div>
+  );
+}

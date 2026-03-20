d473b18a19431063e4c75378c101effcb5e14cd8
Lucas Cavalcante dos Santos <cavalcanteprofissional@outlook.com>
2026-03-20 19:38:45 -0300

    docs: add bug fixes section and remove test page

--- src/app/test/page.tsx
+++
@@ -1,9 +0,0 @@
-export default function TestPage() {
-  return (
-    <div style={{ padding: "40px", textAlign: "center" }}>
-      <h1>🧪 Teste</h1>
-      <p>Se você está vendo esta mensagem, o Next.js está funcionando!</p>
-      <a href="/login" style={{ color: "blue" }}>Ir para Login</a>
-    </div>
-  );
-}
